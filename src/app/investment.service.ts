9d8644dfdc78fb446e1af0359016d9c0f37ef0a8
LuisFerVR <luis.ruiz@ex.gerdau.com>
2024-09-24 14:16:27 -0600

    Se agregó el uso de  señales al servicio.

--- src/app/investment.service.ts
+++ src/app/investment.service.ts
@@ -1,16 +1,16 @@
-import { Injectable } from "@angular/core";
+import { Injectable, signal } from "@angular/core";
 import type { InvestmentInput } from "./investment-input.model";
 
 @Injectable({providedIn:"root"})
 export class InvestmentService{
-    resultData ?: {
+    resultsData = signal< {
         year : number;
         interest : number;
         valueEndOfYear : number;
         annualInvestment : number;
         totalInterest : number;
         totalAmountInvested : number;
-    } [];
+    } [] | undefined>(undefined);
 
     CalculateInvestmentResults(data:InvestmentInput) {
         const annualData = [];
@@ -32,7 +32,7 @@ export class InvestmentService{
           });
         }
       
-        //this.resultsData.set(annualData);
-        this.resultData = annualData;
+        this.resultsData.set(annualData);
+        //this.resultData = annualData;
     }
 }
